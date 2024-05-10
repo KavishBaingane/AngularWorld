3c387f0eff6fd50e87b4f52721d07be8a39d1d2a
KavishBaingane <104kbaingane@gmail.com>
2024-05-10 19:02:19 +0530

    W1:Property Binding.

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -4,13 +4,18 @@ import { Component, OnInit } from '@angular/core';
   //selector: '[app-servers]',
   //select by class downwarss
   selector:'.app-servers',
-  //templateUrl: './servers.component.html',
-  template:`<app-server></app-server>
-             <app-server></app-server>`,
+  templateUrl: './servers.component.html',
+  // template:`<app-server></app-server>
+  //            <app-server></app-server>`,
   styleUrls:['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  constructor(){}
+  allowNewServer=false;
+  constructor(){
+    setTimeout(()=>{
+      this.allowNewServer = true;
+    }, 2000);
+  }
   ngOnInit() {
   }
 }
